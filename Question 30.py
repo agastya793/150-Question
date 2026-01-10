2bcffc173f0415aa4a4894f212701270de76c0f6
Shubham Thakur <agastya071@gmail.com>
2026-01-10 23:30:20 +0530

    Add files via upload

---
+++ Question 30.py
@@ -0,0 +1,12 @@
+'''   1 1 1 1 1 1
+      2 2 2 2 2
+      3 3 3 3
+      4 4 4
+      5 5
+      6     '''
+
+n = 6
+for i in range(1,n+1):
+    for j in range(n-i+1):
+        print(i, end = " ")
+    print()
