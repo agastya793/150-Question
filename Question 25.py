7f1b495642152e789a690472283ca956f0e8277c
Shubham Thakur <agastya071@gmail.com>
2026-01-10 23:28:10 +0530

    Add files via upload

---
+++ Question 25.py
@@ -0,0 +1,23 @@
+'''    *        *
+       **      **
+       ***    ***
+       ****  ****
+       **********
+       ****  ****
+       ***    ***
+       **      **
+       *        *      '''
+
+
+n = 5   # half height
+
+# upper part
+for i in range(1, n + 1):
+    print("*" * i + " " * (2 * (n - i)) + "*" * i)
+
+# lower part
+for i in range(n - 1, 0, -1):
+    print("*" * i + " " * (2 * (n - i)) + "*" * i)
+
+
+
