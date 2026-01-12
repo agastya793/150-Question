5479e3e23f7c96a39ba25b293173cd6158d059b3
Shubham Thakur <agastya071@gmail.com>
2026-01-12 22:33:12 +0530

    Add files via upload

---
+++ # Question 43.py
@@ -0,0 +1,8 @@
+# Write a program to generate multiplication tables for a given number.
+
+num = int(input("enter a number: "))
+
+print("Multiplication table of ", num)
+
+for i in range(1,11):
+    print(num,"x",i,"=",num* i)
