23914be8b018898f07e7ae38d316210948b187f3
Shubham Thakur <agastya071@gmail.com>
2026-01-18 01:00:34 +0530

    Add files via upload

---
+++ Question 96.py
@@ -0,0 +1,8 @@
+# Write a program to create a right-angle triangle pattern with numbers.
+
+n = int(input("enter number of rows: "))
+
+for i in range(1, n+1):
+    for j in range(1,i+1):
+        print(j,end = " ")
+    print()
