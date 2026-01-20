a60ac6fc263c7980ace383540368da3b91dcca41
Shubham Thakur <agastya071@gmail.com>
2026-01-20 18:12:51 +0530

    Add files via upload

---
+++ Question 120.py
@@ -0,0 +1,11 @@
+# program to shuffle deck of cards
+
+import random, itertools
+
+deck = list(itertools.product(range(1,14),["spade","club","hearts","diamond"]))
+
+random.shuffle(deck)
+print(deck)
+
+for i in range(5):
+    print(deck[i][0], "of" , deck[i][1])
