9ea7e6794d0e73818addbf8a70f182b12a521dd2
Shubham Thakur <agastya071@gmail.com>
2026-01-23 17:41:19 +0530

    Add files via upload

---
+++ Question 145.py
@@ -0,0 +1,10 @@
+# Two Sum Problem
+
+def two_sum(nums, target):
+    d = {}
+    for i, n in enumerate(nums):
+        if target - n in d:
+            return [d[target - n], i]
+        d[n] = i
+
+print(two_sum([2,7,11,15], 9))
